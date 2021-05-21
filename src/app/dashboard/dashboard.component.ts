b26f5eef13a90215797820f9b9a9a75d62fb69c5
annasaandthrajose <annasaandthrajose@gmail.com>
2021-05-21 21:30:12 +0530

    login and dash board reactive and validated

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -7,36 +8,88 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-dAccno="";
+//dAccno="";
 dPswd="";
 dAmount="";
 wAccno="";
 wPswd="";
 wAmount=""
+depositForm=this.fb.group({
+  dAccno:['',[Validators.required,Validators.minLength(4),Validators.pattern('[0-9]*')]],
+  dPswd:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]],
+  dAmount:['',[Validators.required,Validators.pattern('[0-9]*')]]
 
-  constructor(private dataService:DataService) { }
+})
+withdrawForm=this.fb.group({
+  wAccno:['',[Validators.required,Validators.minLength(4),Validators.pattern('[0-9]*')]],
+  wPswd:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]],
+  wAmount:['',[Validators.required,Validators.pattern('[0-9]*')]]
+})
+
+
+
+  constructor(private dataService:DataService,private fb:FormBuilder) { 
+    
+  }
 
   ngOnInit(): void {
   }
 deposit(){
-  //alert("Amount Credited")
-  var accno=this.dAccno;
-  var pswd=this.dPswd;
-  var amount=this.dAmount;
+  if(this.depositForm.valid){//call deposit function line 48 to 55
+    var accno=this.depositForm.value.dAccno;
+  var pswd=this.depositForm.value.dPswd
+  var amount=this.depositForm.value.dAmount;
   const result=this.dataService.deposit(accno,pswd,amount)
   if(result)
   {
     alert("the given amount is"+amount+"credited and new balance is"+result)
   }
+  }
+  //alert("Amount Credited")
+  //var accno=this.dAccno;
+  //var pswd=this.dPswd;
+  //var amount=this.dAmount;
+  //var accno=this.depositForm.value.dAccno;
+  //var pswd=this.depositForm.value.dPswd;
+  //var amount=this.depositForm.value.dAmount;
+  //const result=this.dataService.deposit(accno,pswd,amount)
+  //if(result)
+ // {
+    //alert("the given amount is"+amount+"credited and new balance is"+result)
+  //}
+  else{
+    alert("Invalid Form")
+  }
 }
 withdraw(){
-  var accno=this.wAccno;
-  var pswd=this.wPswd;
-  var amount=this.wAmount;
+  if(this.withdrawForm.valid)
+  //valid copy 82 to 91 lines
+  {
+    var accno=this.withdrawForm.value.wAccno;
+  var pswd=this.withdrawForm.value.wPswd;
+  var amount=this.withdrawForm.value.wAmount;
+
   const result=this.dataService.withdraw(accno,pswd,amount)
   if(result)
   {
     alert("the given amount is"+amount+"debited and new balance is"+result)
   }
 }
+else{
+  alert("Invalid Form")
+}
+  }
+  //var accno=this.wAccno;
+ // var pswd=this.wPswd;
+  //var amount=this.wAmount;
+  //var accno=this.withdrawForm.value.wAccno;
+  //var pswd=this.withdrawForm.value.wPswd;
+  //var amount=this.withdrawForm.value.wAmount;
+
+  //const result=this.dataService.withdraw(accno,pswd,amount)
+  //if(result)
+  //{
+    //alert("the given amount is"+amount+"debited and new balance is"+result)
+  //}
+//}
 }
